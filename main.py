98f889976a1706eb90accd5a247ec7542e41ccc2
Baptiste Choquet <baptiste.choquet@pm.me>
2020-12-01 16:35:40 +0100

    Commit DilateV2

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
+import os
 import cv2
 import nb
 import blur
 import dilate
+
+path = os.getcwd()
+print ("The current working directory is %s" % path)
+
+path = "Data/output"
+
+try:
+    os.mkdir(path)
+except OSError:
+    print ("Creation of the directory %s failed" % path)
+else:
+    print ("Successfully created the directory %s " % path)
 
 nb.transnb()
 blur.transblur()
