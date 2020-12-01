afd62787bc09fa3a1fbe79fdd36e6acde3c3b9cb
Baptiste Choquet <baptiste.choquet@pm.me>
2020-12-01 15:50:15 +0100

    Commit BlurV1

--- main.py
+++ main.py
@@ -8,6 +8,11 @@ cv2.imshow('Gray image', gray)
 
 cv2.imwrite("Data/output/schtroumpf_grey.jpg", gray)
 
-cv2.waitKey(0)
+src = cv2.imread('Data/imgs/schtroumpf.jpg', cv2.IMREAD_UNCHANGED)
+
+blur = cv2.GaussianBlur(src,(5,5),0)
+cv2.imshow("Blured Image", blur)
+cv2.imwrite("Data/output/schtroumpf_blured.jpg", blur)
+
 cv2.destroyAllWindows()
 
