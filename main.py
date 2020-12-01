573388ea4cc0d8eed25004ed48ce98ef635a6ac2
Baptiste Choquet <baptiste.choquet@pm.me>
2020-12-01 15:20:49 +0100

    Commit NBV1

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
-# This is a sample Python script.
+import cv2
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+image = cv2.imread("Data/imgs/schtroumpf.jpg")
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
+cv2.imshow('Original image', image)
+cv2.imshow('Gray image', gray)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+cv2.waitKey(0)
+cv2.destroyAllWindows()
