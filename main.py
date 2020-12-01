f4f0ec6e635083dbcce7af7ddf2945af741202f8
Baptiste Choquet <baptiste.choquet@pm.me>
2020-12-01 15:34:54 +0100

    Commoit NBV2

--- main.py
+++ main.py
@@ -6,5 +6,8 @@ gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 cv2.imshow('Original image', image)
 cv2.imshow('Gray image', gray)
 
+cv2.imwrite("Data/output/schtroumpf_grey.jpg", gray)
+
 cv2.waitKey(0)
 cv2.destroyAllWindows()
+
